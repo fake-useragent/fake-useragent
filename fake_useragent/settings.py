f0497f4fb598cc9fa6688ffcfcb1dfa1d343cbbf
hellysmile <hellysmile@gmail.com>
2015-11-06 20:05:41 +0200

    Make isort happy.

--- fake_useragent/settings.py
+++ fake_useragent/settings.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, unicode_literals
 import os
 import tempfile
 
-
 DB = os.path.join(tempfile.gettempdir(), 'fake_useragent.json')
 
 BROWSERS_STATS_PAGE = 'http://www.w3schools.com/browsers/browsers_stats.asp'
