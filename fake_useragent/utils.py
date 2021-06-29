b608f96232874c32cd5e878f12dbe1a5bb5c8eaa
Jordan9675 <jordan.vuong96@gmail.com>
2021-06-29 18:35:02 +0200

    Modified table class name for non-cached UserAgent

--- fake_useragent/utils.py
+++ fake_useragent/utils.py
@@ -96,7 +96,7 @@ def get_browsers(verify_ssl=True):
     """
     html = get(settings.BROWSERS_STATS_PAGE, verify_ssl=verify_ssl)
     html = html.decode('utf-8')
-    html = html.split('<table class="w3-table-all notranslate">')[1]
+    html = html.split('<table class="ws-table-all notranslate">')[1]
     html = html.split('</table>')[0]
 
     pattern = r'\.asp">(.+?)<'
