11c7d728a063a1dc5d194da20f05734961bcbf97
Melroy van den Berg <melroy@melroy.org>
2022-04-01 00:01:59 +0200

    Prepare GitHub Actions. Only support Python3

--- setup.py
+++ setup.py
@@ -7,30 +7,25 @@ import re
 
 from setuptools import setup
 
-
 def get_version():
     regex = r"__version__\s=\s\'(?P<version>[\d\.]+?)\'"
-
     path = ('fake_useragent', 'settings.py')
-
     return re.search(regex, read(*path)).group('version')
-
 
 def read(*parts):
     filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts)
-
     with io.open(filename, encoding='utf-8', mode='rt') as fp:
         return fp.read()
-
 
 setup(
     name='fake-useragent',
     version=get_version(),
-    author='hellysmile@gmail.com',
-    author_email='hellysmile@gmail.com',
-    url='https://github.com/hellysmile/fake-useragent',
+    author='Melroy van den Berg',
+    author_email='melroy@melroy.org',
+    url='https://github.com/danger89/fake-useragent',
     description='Up to date simple useragent faker with real world database',
-    long_description=read('README.rst'),
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     packages=[str('fake_useragent')],
     include_package_data=True,
     zip_safe=False,
@@ -42,7 +37,6 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
     ],
