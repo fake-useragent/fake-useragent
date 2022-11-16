3b7dfe0ee5a800af738f8ead64ea95376b492b16
Melroy van den Berg <melroy@melroy.org>
2022-11-16 01:17:25 +0100

    Reintroduce pre Python 3.0 support (oops)

--- src/fake_useragent/settings.py
+++ src/fake_useragent/settings.py
@@ -1,6 +1,11 @@
 import os
 import tempfile
-from importlib import metadata
+
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata
 
 __version__ = metadata.version("fake-useragent")
 
