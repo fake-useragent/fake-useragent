8cfb62ab4b4df13500a21c1d32ae0f95b0641aaf
Sebastian Correa <43179146+sebastian-correa@users.noreply.github.com>
2024-10-08 13:26:56 -0300

    Enable D & DOC ruff rules with Google convention (#365)

--- src/fake_useragent/__init__.py
+++ src/fake_useragent/__init__.py
@@ -1,3 +1,5 @@
+"""Up-to-date simple useragent faker with real world database."""
+
 from fake_useragent.errors import FakeUserAgentError, UserAgentError
 from fake_useragent.fake import FakeUserAgent, UserAgent
 from fake_useragent.settings import __version__ as VERSION
--- src/fake_useragent/log.py
+++ src/fake_useragent/log.py
@@ -1,3 +1,5 @@
+"""Defines a common logger for the library."""
+
 import logging
 
 logger = logging.getLogger(__package__)
