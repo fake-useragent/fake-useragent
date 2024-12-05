0310d235004ddc37ccaf23ec4cb2ac33b8dc18c2
Muspi Merol <me@promplate.dev>
2024-12-05 23:38:15 +0800

    Specify `__all__` in `__init__.py`

--- src/fake_useragent/__init__.py
+++ src/fake_useragent/__init__.py
@@ -3,3 +3,11 @@
 from fake_useragent.errors import FakeUserAgentError, UserAgentError
 from fake_useragent.fake import FakeUserAgent, UserAgent
 from fake_useragent.get_version import __version__
+
+__all__ = [
+    "FakeUserAgent",
+    "UserAgent",
+    "FakeUserAgentError",
+    "UserAgentError",
+    "__version__",
+]
