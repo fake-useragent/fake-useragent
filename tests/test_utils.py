0e28b9ee087ce65bad4e6a5a8f14837dce89cbeb
Muspi Merol <me@promplate.dev>
2024-12-15 23:23:49 +0800

    Validate loaded data with `pydantic` instead of hard-coded type checks

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,6 +6,7 @@ from importlib import invalidate_caches
 from pathlib import Path
 from shutil import make_archive
 from tempfile import TemporaryDirectory
+from typing import List
 
 from fake_useragent import utils
 
@@ -20,18 +21,18 @@ class TestUtils(unittest.TestCase):
     def test_utils_load(self):
         data = utils.load()
 
-        self.assertIsInstance(data, list)
         self.assertGreater(len(data), 1000)
-        self.assertIsInstance(data[0], object)
-        self.assertIsInstance(data[0]["percent"], float)
-        self.assertIsInstance(data[0]["type"], str)
-        self.assertIsInstance(data[0]["device_brand"], str)
-        self.assertIsInstance(data[0]["browser"], str)
-        self.assertIsInstance(data[0]["browser_version"], str)
-        self.assertIsInstance(data[0]["browser_version_major_minor"], float)
-        self.assertIsInstance(data[0]["os"], str)
-        self.assertIsInstance(data[0]["os_version"], str)
-        self.assertIsInstance(data[0]["platform"], str)
+
+        if sys.version_info < (3, 12):
+            from pytest import skip
+
+            skip(
+                "pydantic only supports `typing_extensions.TypedDict` instead of `typing.TypedDict` on Python < 3.12."
+            )
+
+        from pydantic import TypeAdapter
+
+        TypeAdapter(List[utils.BrowserUserAgentData]).validate_python(data, strict=True)
 
     def test_utils_load_from_zipimport(self):
         with make_temporary_directory() as temp_dir:
@@ -53,18 +54,7 @@ class TestUtils(unittest.TestCase):
 
             data = utils.load()
 
-        self.assertIsInstance(data, list)
         self.assertGreater(len(data), 1000)
-        self.assertIsInstance(data[0], object)
-        self.assertIsInstance(data[0]["percent"], float)
-        self.assertIsInstance(data[0]["type"], str)
-        self.assertIsInstance(data[0]["device_brand"], str)
-        self.assertIsInstance(data[0]["browser"], str)
-        self.assertIsInstance(data[0]["browser_version"], str)
-        self.assertIsInstance(data[0]["browser_version_major_minor"], float)
-        self.assertIsInstance(data[0]["os"], str)
-        self.assertIsInstance(data[0]["os_version"], str)
-        self.assertIsInstance(data[0]["platform"], str)
 
         # cleanup
         unload_module("fake_useragent")
